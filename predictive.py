c933ebce0160fd3246ce1840366e49400690f261
Hannah Windgasse <hannah.windgasse@web.de>
2023-07-09 00:42:12 +0200

    continue task4

--- predictive.py
+++ predictive.py
@@ -1,6 +1,16 @@
 # methods defined for Task4 - Predictive
 
-----------------------------------------------------------------------------------------------------------------------------------
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn import metrics
+from sklearn.model_selection import RandomizedSearchCV, RepeatedStratifiedKFold
+import math
+import pandas as pd
+
+#################################################################################################################################
 
 # method for creating the categorical und numerical features:
 
@@ -12,8 +22,8 @@ def cat_and_num(spatial_feature):
     
     return categoric, numeric
 
-----------------------------------------------------------------------------------------------------------------------------------
-    
+#################################################################################################################################
+     
 # method for transforming the days and season variable to integer:
 
 def dayandseas_to_int(df):
@@ -24,7 +34,7 @@ def dayandseas_to_int(df):
 
     return df
 
-----------------------------------------------------------------------------------------------------------------------------------
+#################################################################################################################################
 
 # method for creating a pipeline:
 
@@ -46,7 +56,7 @@ def pipeline_for_prediction(categoric, numeric, model):
     )
     return pipeline
 
-----------------------------------------------------------------------------------------------------------------------------------
+#################################################################################################################################
 
 # method for creating scores based on a specific dataframe and feature (depended variable):
 
@@ -56,25 +66,24 @@ def get_prediction_scores(y_true, y_predicted, df, depend_feat):
     print(f"MAE: {metrics.mean_absolute_error(y_true, y_predicted): .3f}")
     print(f"MSE: {metrics.mean_squared_error(y_true, y_predicted): .3f}")
     print(f"RMSE: {math.sqrt(metrics.mean_squared_error(y_true, y_predicted)): .3f}")
-    print(f"Accuracy:", round((1-(metrics.mean_absolute_error(y_true, y_predicted)/df[dep_feat].mean()))*100,2),
+    print(f"Accuracy:", round((1-(metrics.mean_absolute_error(y_true, y_predicted)/df[depend_feat].mean()))*100,2),
           "%")
     print(f"R2: {100 * metrics.r2_score(y_true, y_predicted): .3f} %")
     print(f"Max Residual Error: {metrics.max_error(y_true, y_predicted): .3f}")
 
-
-----------------------------------------------------------------------------------------------------------------------------------
+#################################################################################################################################
 
 # method whole prediction:
 
 # function for creating pipeline and fitting model (created by the pipeline), predict and printing scores
-def pipeline_fit_predict(reg, categoric, numeric, x_train, y_train, x_val, y_val):
+def pipeline_fit_predict(reg, categoric, numeric, x_train, y_train, x_val, y_val, df, depend_feat):
     pipeline = pipeline_for_prediction(categoric, numeric, reg)
     pipeline.fit(x_train, y_train)
     y_predict = pipeline.predict(x_val)
     get_prediction_scores(y_val, y_predict, df, depend_feat)
 
----------------------------------------------------------------------------------------------------------------------------------
-
+#################################################################################################################################
+    
 # method for performing RandomizedSearchCV for hyperparameter tuning:
 
 # function for finding the best hyperparameter by using RandomizedSearchCV and RepeatedStratifiedKFold
@@ -89,9 +98,6 @@ def pipeline_fit_predict(reg, categoric, numeric, x_train, y_train, x_val, y_val
    - n_splits: number of splits in RepeatedStratifiedKFold
    - verbose: getting information during the grid search
 """
-
-
-from sklearn.model_selection import RandomizedSearchCV, RepeatedStratifiedKFold
 
 def find_best_hyperparameters(pipeline, x_val, y_val, model_par, score, n_iter = 50,  
                                    n_repeats=3, n_splits=5, n_jobs=1, verbose=True): #  n_repeats=3
@@ -112,12 +118,12 @@ def find_best_hyperparameters(pipeline, x_val, y_val, model_par, score, n_iter =
     print("The best hyper parameters for the model are:")
     print(grid_pipeline.best_params_)
 
----------------------------------------------------------------------------------------------------------------------------------
+################################################################################################################################# 
 
 # method for performing a split for train, test and validation set:
 
 # default: 30% test, 20% validation, 30% training (and 70% train_val)
-def train_val_test(df,testsize=0.3,valsize=0.2):
+def train_val_test(df, testsize=0.3, valsize=0.2):
     
     #split the data set in (1-testsize)% training set and testsize% testing set
     x_train, x_test, y_train, y_test = train_test_split(df.drop('numOfTaxis_area', axis=1)
@@ -134,15 +140,17 @@ def train_val_test(df,testsize=0.3,valsize=0.2):
     
     return x_train_val, y_train_val, x_train, y_train, x_val, y_val, x_test, y_test 
 
+#################################################################################################################################
 
+# method for sampling data, when its already splittet:
 
-
+# x_set and y_set are the already splitted datasets, which should be sampled
 def sampling_already_splitted(x_set, y_set, dependend, num_samples):
     
-    y_val_df = y_set.to_frame()
-    df = pd.concat([x_set, y_val_df], axis=1)
-    df_sample = df.sample(num_samples)
-    y_val = df_sample[dependend]
+    y_val_df = y_set.to_frame() # y_set is series, so turn it to frame
+    df = pd.concat([x_set, y_val_df], axis=1) # concat to whole df
+    df_sample = df.sample(num_samples) # sample the df
+    y_val = df_sample[dependend] # again split in y and x set
     x_val = df_sample.drop(columns = dependend)
     
     return x_val, y_val
